9412f77fe1614d880503866610c02a48f874e50d
Peter Jokumsen <pjokumsen@gmail.com>
2023-09-13 20:01:44 +0200

    remove filler component

--- apps/hpb-client/src/app/app.component.ts
+++ apps/hpb-client/src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { NxWelcomeComponent } from './nx-welcome.component';
-
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'helderberg-pirates-baseball-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'hpb-client';
-}
+export class AppComponent {}
