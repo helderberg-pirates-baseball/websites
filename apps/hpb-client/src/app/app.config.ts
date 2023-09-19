39281586d72faef4e896d050c5978fdd8aec3f55
Peter Jokumsen <pjokumsen@gmail.com>
2023-09-19 07:25:46 +0200

    feat(hpb-client): add pwa 🍰  (#32)
    ## Description 📝
    
    Add PWA, to allow caching scripts.
    
    ## Checklist ✅
    
    - [ ] I've forked this repository.
    - [x] I've created a new branch for this PR.
    - [x] My code follows the project's coding style.
    - [x] I've tested my changes thoroughly.
    - [x] My changes don't introduce any new warnings or errors.
    - [ ] I've updated the documentation if necessary.
    - [x] My PR has a fun emoji in its title. 🎈

--- apps/hpb-client/src/app/app.config.ts
+++ apps/hpb-client/src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
