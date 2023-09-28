d122c7491df092cf564aad3b0487becfd40d7af3
Peter Jokumsen <pjokumsen@gmail.com>
2023-09-28 23:02:44 +0200

    feat: add theme and angular material 😍  (#41)
    <!--
    Hey there! 👋 Thank you for contributing to our awesome project! 🎉
    Before you submit your pull request, please make sure to read our
    guidelines below. 😄
    -->
    
    ## Description 📝
    
    Created shared-styles lib to contain theme for any applications in the
    mono-repo.

--- apps/hpb-client/src/app/app.component.ts
+++ apps/hpb-client/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatCardModule, MatIconModule],
   selector: 'helderberg-pirates-baseball-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
