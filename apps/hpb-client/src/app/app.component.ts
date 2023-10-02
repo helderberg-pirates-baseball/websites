3f480b758672f10897ff28064f6d38a0f9a06fcc
Peter Jokumsen <pjokumsen@gmail.com>
2023-10-02 23:33:37 +0200

    feat(hpb-client): update to allow toggle for dark-mode

--- apps/hpb-client/src/app/app.component.ts
+++ apps/hpb-client/src/app/app.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { HpbThemeService } from '@helderberg-pirates-baseball/shared/styles';
+import { IconMenuComponent } from '@helderberg-pirates-baseball/shared/ui';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatCardModule, MatIconModule],
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    IconMenuComponent,
+    MatMenuModule,
+    MatSlideToggleModule,
+    FormsModule,
+  ],
   selector: 'helderberg-pirates-baseball-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private _darkMode = false;
+  private _theme = inject(HpbThemeService);
+
+  get darkMode(): boolean {
+    return this._darkMode;
+  }
+
+  set darkMode(value: boolean) {
+    console.log('darkMode', value);
+    this._theme.toggleTheme();
+  }
+
+  ngOnInit(): void {
+    this._theme.init();
+  }
+}
--- apps/hpb-client/src/app/app.config.ts
+++ apps/hpb-client/src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 
@@ -7,6 +8,7 @@ import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideAnimations(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
