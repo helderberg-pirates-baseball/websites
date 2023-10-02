8375046253ad27086a73db2b3a53e30b407f0978
Peter Jokumsen <pjokumsen@gmail.com>
2023-10-02 23:08:14 +0200

    feat(shared-ui): create hpb-theme service

---
+++ libs/shared/styles/src/lib/hpb-theme.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HpbThemeService } from './hpb-theme.service';
+
+describe('HpbThemeService', () => {
+  let service: HpbThemeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+
+    service = TestBed.inject(HpbThemeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ libs/shared/styles/src/lib/hpb-theme.service.ts
@@ -0,0 +1,40 @@
+import { DOCUMENT } from '@angular/common';
+import { inject, Injectable } from '@angular/core';
+
+import { HpbTheme } from './models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HpbThemeService {
+  private readonly _lightThemeClass = 'light-theme';
+
+  private _document: Document = inject(DOCUMENT);
+
+  private getTheme(): HpbTheme {
+    const storedTheme = localStorage.getItem('hpb-theme');
+    if (storedTheme) {
+      return storedTheme as HpbTheme;
+    }
+
+    const darkModeMatch = window.matchMedia('(prefers-color-scheme: dark)');
+    return darkModeMatch.matches ? 'dark' : 'light';
+  }
+
+  private setTheme(theme: HpbTheme): void {
+    localStorage.setItem('hpb-theme', theme);
+    this._document.body.classList.remove(this._lightThemeClass);
+    if (theme === 'light') {
+      this._document.body.classList.add(this._lightThemeClass);
+    }
+  }
+
+  init(): void {
+    this.setTheme(this.getTheme());
+  }
+
+  toggleTheme(): void {
+    const theme = this.getTheme();
+    this.setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+}
