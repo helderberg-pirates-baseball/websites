238d846f935beef3055afa58766d726aac4516de
Peter Jokumsen <pjokumsen@gmail.com>
2023-10-06 22:36:31 +0200

    fix(hpb-client): get spec passing

--- apps/hpb-client/src/app/app.component.spec.ts
+++ apps/hpb-client/src/app/app.component.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HpbThemeService } from '@helderberg-pirates-baseball/shared/styles';
 
 import { AppComponent } from './app.component';
 
+import Mocked = jest.Mocked;
+
 describe('AppComponent', () => {
+  let hpbThemeSpy: Mocked<HpbThemeService>;
+
   beforeEach(async () => {
+    hpbThemeSpy = {
+      init: jest.fn(() => 'dark'),
+    } as unknown as Mocked<HpbThemeService>;
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterTestingModule],
+      providers: [
+        // keep split
+        { provide: HpbThemeService, useValue: hpbThemeSpy },
+      ],
     }).compileComponents();
   });
 
