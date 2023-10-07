af2b3415cda7b58fd25a4c735228d14bbb3bf720
Peter Jokumsen <pjokumsen@gmail.com>
2023-10-07 08:58:04 +0200

    chore: fix lint

--- apps/hpb-client/src/app/app.component.spec.ts
+++ apps/hpb-client/src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { HpbThemeService } from '@helderberg-pirates-baseball/shared/styles';
 
 import { AppComponent } from './app.component';
