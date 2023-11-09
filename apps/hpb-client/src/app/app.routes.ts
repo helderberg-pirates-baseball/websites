14ef592f1f190e0dc2fb9bc201b3b37e8a78cc35
Peter Jokumsen <pjokumsen@gmail.com>
2023-11-09 23:01:24 +0200

    fix(workflows): force cache up to date 🚤  (#79)
    ## Description 📝
    
    Update workflow to force nx cache stays in sync

--- apps/hpb-client/src/app/app.routes.ts
+++ apps/hpb-client/src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'home',
-    loadChildren: () => import('@helderberg-pirates-baseball/pages/landing').then((m) => m.pagesLandingRoutes),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('@helderberg-pirates-baseball/pages/landing').then((m) => m.pagesLandingRoutes),
   },
   {
     path: '**',
