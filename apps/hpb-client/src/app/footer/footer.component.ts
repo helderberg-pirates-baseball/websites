e74109fc7ef63a4f8ace46be1dcec4b7db243c00
Peter Jokumsen <pjokumsen@gmail.com>
2023-11-18 19:52:20 +0200

    feat(hpb-client): update footer

--- apps/hpb-client/src/app/footer/footer.component.ts
+++ apps/hpb-client/src/app/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'helderberg-pirates-baseball-footer',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatTooltipModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
